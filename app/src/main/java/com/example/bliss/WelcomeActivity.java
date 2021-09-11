d5ca5877dbf8e0aed9cc076628d6dc1f6fc683b6
guanie <43637539+guanie@users.noreply.github.com>
2021-09-11 16:58:14 +1000

    First Commit

---
+++ app/src/main/java/com/example/bliss/WelcomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.bliss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
